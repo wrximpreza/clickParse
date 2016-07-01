0e3568dc5dbd151e96a04b4626908e2202fb980e
Ivan Dyachuk <wrximpreza1987@gmail.com>
2016-07-01 21:43:10 +0300

    update code

--- app/controller/urlFind.php
+++ app/controller/urlFind.php
@@ -32,6 +32,14 @@ class urlFind implements parserInterface
     public function __construct($msg, $log, $word, $limit = 1)
     {
         $this->word = $word;
+        $this->word = explode(PHP_EOL, $word);
+
+        foreach ($this->word as $w){
+            if($w == ''){
+                unset($w);
+            }
+        }
+
         $this->limit = $limit;
         if($this->limit>999){
             $this->limit = 999;
@@ -60,54 +68,58 @@ class urlFind implements parserInterface
         try {
 
             $pageNum = 0;
-            if($this->limit>100) {
 
-                $count = ceil($this->limit / 100);
-                //$subCount = $this->limit - (100 * $count);
-            }else{
-                $count = 1;
-            }
 
             $lists = array();
-
-            for ($pageNum = 0; $pageNum < $count; $pageNum++) {
-
-                $url = 'http://www.google.ru/search?q=' . urlencode($this->word) . '&num=' . $this->countShow . '&hl=ru&start=' . $pageNum . '&ie=UTF-8';
-
-
-                $page = file_get_contents($url);
-
-                if (!strpos($http_response_header[0], "200")) {
-                    //$this->msg->error('Страница вернула код '.$http_response_header[0]);
-                    $this->log->error('Страница вернула код ' . $http_response_header[0]);
-                    return 'Страница вернула код ' . $http_response_header[0];
+            foreach ( $this->word as $w) {
+                if($this->limit>100) {
+                    $count = ceil($this->limit / 100);
+                    //$subCount = $this->limit - (100 * $count);
+                }else{
+                    $count = 1;
                 }
 
-                if (!$page)
-                    $page = curlgoogle($url);
+                for ($pageNum = 0; $pageNum < $count; $pageNum++) {
 
-                if (!$page) {
-                    //$this->msg->error('Страница не загрузилась');
-                    $this->log->error('Страница не загрузилась');
-                    //return 'Страница не загрузилась';
-                } else {
-
-                    if (preg_match_all('#<cite>(.+?)</cite>#si', $page, $match)) {
+                    $url = 'http://www.google.ru/search?q=' . urlencode($w) . '&num=' . $this->countShow . '&hl=ru&start=' . $pageNum . '&ie=UTF-8';
 
 
-                        foreach ($match['1'] as $v) {
-                            $lists[] = urlencode(strip_tags($v));
-                        }
+                    $page = file_get_contents($url);
 
-                        //$this->msg->info('Данные спарсились по слову "' . $this->word . '" с google.com');
-
-                    } else {
-                        //$this->msg->error('По запросу "' . $this->word . '" линков в google.com нет.');
-                        $this->log->error('По запросу "' . $this->word . '" линков в google.com нет.');
+                    if (!strpos($http_response_header[0], "200")) {
+                        //$this->msg->error('Страница вернула код '.$http_response_header[0]);
+                        $this->log->error('Страница вернула код ' . $http_response_header[0]);
+                        return 'Страница вернула код ' . $http_response_header[0];
                     }
 
-                    //return 'По запросу "' . $this->word . '" линков в google.com нет.';
+                    if (!$page)
+                        $page = curlgoogle($url);
+
+                    if (!$page) {
+                        //$this->msg->error('Страница не загрузилась');
+                        $this->log->error('Страница не загрузилась');
+                        //return 'Страница не загрузилась';
+                    } else {
+
+                        if (preg_match_all('#<cite>(.+?)</cite>#si', $page, $match)) {
+
+
+                            foreach ($match['1'] as $v) {
+                                $lists[] = urlencode(strip_tags($v));
+                            }
+
+                            //$this->msg->info('Данные спарсились по слову "' . $this->word . '" с google.com');
+
+                        } else {
+                            //$this->msg->error('По запросу "' . $this->word . '" линков в google.com нет.');
+                            $this->log->error('По запросу "' . $this->word . '" линков в google.com нет.');
+                        }
+
+                        //return 'По запросу "' . $this->word . '" линков в google.com нет.';
+                    }
+
                 }
+
 
             }
 
@@ -131,8 +143,12 @@ class urlFind implements parserInterface
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_PROXY, '88.87.72.72:8080');
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 300);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_USERAGENT, '');
+        curl_setopt($curl, CURLOPT_REFERER, 'http://www.google.ru/');
         return curl_exec($curl);
     }
 
